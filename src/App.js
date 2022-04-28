5510a01bbcdfa6dfa40589809dde0a8db6c265e3
charlesmcdougal <charlesmcdougal@gmail.com>
2022-04-28 21:40:28 +0300

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+
+import { Routes, Route, Link, Navigate } from 'react-router-dom'
+
+import Comments from './components/comments/Comments'
+import Layout from './components/layout/Layout';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+//----ROUTES----//
+const Quotes = React.lazy(() => import('./routes/Quotes'))
+const AddQuote = React.lazy(() => import('./routes/AddQuote'))
+const Quote = React.lazy(() => import('./routes/Quote'))
+const NotFound = React.lazy(() => import('./routes/NotFound'))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Suspense fallback={<div className='centered'><LoadingSpinner /></div>}>
+        <Routes>
+          <Route path='/' element={<Navigate replace to='/quotes' />} />
+          <Route path='/quotes/:quoteId' element={<Quote />}>
+            <Route path='/quotes/:quoteId' element=
+              {
+                <div className='centered'>
+                  <Link className='btn--flat' to={'comments'}>Load Comments</Link>
+                </div>
+              } 
+            />
+            <Route path='/quotes/:quoteId/comments' element={<Comments />} />
+          </Route>
+          <Route path='/quotes' element={<Quotes />} />
+          <Route path='/add-quote' element={<AddQuote />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </Layout>
   );
 }
 
